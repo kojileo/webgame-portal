39629c4d89212502fda1f6761fd783e9f26e0184
kojileo <kojileo>
2024-09-22 16:48:00 +0900

    微修正

--- webgame-portal-backend/src/app.ts
+++ webgame-portal-backend/src/app.ts
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/games", gameRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
